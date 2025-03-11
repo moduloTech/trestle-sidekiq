7c794e7b47f79f4536969a03cca0e8c13a488e73
Matthieu Ciappara <ciappa_m@modulotech.fr>
2025-03-11 12:17:58 +0100

    Ensure precompilation only if Sprockets is defined
    This change adds a condition to check for Sprockets before appending assets to the precompile list. It prevents potential errors in environments where Sprockets is not available or not used.

--- lib/trestle/sidekiq/engine.rb
+++ lib/trestle/sidekiq/engine.rb
@@ -1,7 +1,7 @@
 module Trestle
   module Sidekiq
     class Engine < ::Rails::Engine
-      config.assets.precompile << "trestle/sidekiq.css"
+      config.assets.precompile << "trestle/sidekiq.css" if defined?(Sprockets)
     end
   end
 end
